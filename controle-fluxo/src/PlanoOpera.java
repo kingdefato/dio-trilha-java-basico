16d3ab8ae7b529d5b791f74878d4654381f93307
kingdefato <ytallobrito2010@gmail.com>
2024-08-27 08:12:29 -0300

    Estruturas Condicionais

---
+++ controle-fluxo/src/PlanoOpera.java
@@ -0,0 +1,20 @@
+public class PlanoOpera {
+    public static void main(String[] args) {
+        String plano =  "T"; //M /T
+
+        switch (plano)
+        {
+            case "T":{
+                System.out.println("5Gb Youtube");
+            }
+                
+            case "M" : {
+                System.out.println("Whats e instagram grátis");
+            }
+            
+            case "B": {
+             System.out.println("100 Minutos de ligação");   
+            }
+        }
+    }
+}
